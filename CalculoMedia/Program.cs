6d63d60c5b62a0f87221b8e6be66d85f13d2ac24
Maiki <maikif37@outlook.com>
2026-03-04 02:11:55 -0300

    teste 2

---
+++ CalculoMedia/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um numero: ");
+int x = int.Parse(Console.ReadLine());
+
+if(x % 2 == 0)
+{
+    Console.WriteLine("numero é PAR");
+}
+else
+{
+    Console.WriteLine("o numero é IMPAR!!");
+}
